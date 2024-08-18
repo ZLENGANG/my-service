bb6acb358ba9be71b4b0e52e6eb841d5d6f23247
roninz <1437004632@qq.com>
2024-08-18 22:38:36 +0800

    修改请求地址

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { getIPAddress } from "../../server/utils/index.js";
 import { BACK_END_PORT } from "../../config.js";
 
 const $http = axios.create({
-  baseURL: `http://${getIPAddress()}:${BACK_END_PORT}`,
+  baseURL: `http://${location.hostname}:${BACK_END_PORT}`,
   timeout: 1000 * 60,
 });
 
